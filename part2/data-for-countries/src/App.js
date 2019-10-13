ebed8a6f777068759d21d0abaa6c24111f0d5923
ranabhat <paribessh_sab@hotmail.com>
2019-10-14 00:14:42 +0300

    data for countries assignment partial set up complete

---
+++ part2/data-for-countries/src/App.js
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Weather from './components/Weather'
+
+const Filter = ({ filterLabel, handleSearchChange }) => {
+  return(
+    <div>
+    <p>{filterLabel} <input onChange={handleSearchChange} /></p>
+    </div>
+  )
+}
+const CountryFull = ({ country }) => {
+  return (
+    <div key={country.name}>
+    <h1>{country.name}</h1>
+    <p>capital {country.capital} </p>
+    <p>population {country.population}</p>
+    <h3>languages</h3>
+    <ul>
+        {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
+    </ul>
+    <img alt="country-flag" width="200" height="200" src={country.flag}></img>
+    
+     <Weather capital={country.capital}/> 
+
+</div>
+     
+ )
+}
+const CountryLess = ({ country, handleShowClick }) => {
+  return(
+    <div key={country.name}>
+    {country.name}
+    <button onClick={handleShowClick}>show</button>
+    </div>
+  )
+}
+const Countries = ({ countries }) => {
+
+  const values = (countries.length > 10) 
+    ? 'Too many matches, specify another filter'
+    : countries
+  return(
+    <div>
+    {values}
+    </div>
+  )
+
+}
+
+const App = () => {
+  //const [newsearch, setNewSearch] = useState('')
+  const [country, setCountry] = useState([])
+  const [newSearch, setNewSearch] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+    // Fetching data from server
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountry(response.data)
+      })
+  }, [])
+
+  const countryToShow = showAll
+      ? country
+      : country.filter(country => country.name.toLowerCase().indexOf(newSearch.toLowerCase()) !== -1)
+//   console.log((countryToShow.length > 10) ? 'too many' : countryToShow)
+// //  console.log('country', countryToShow)
+
+  const handleSearchChange = (event) => {
+    //event.preventDefault()
+    setNewSearch(event.target.value)
+    setShowAll(false)
+  }
+
+  const handleShowClick = (name) => () => {
+   // console.log('name show pressed', name)
+    const whichCountry = countryToShow.filter(country => country.name === name)
+   // setCountry(countryToShow.filter(country => country.name === name))
+   console.log('which county', whichCountry)
+   setCountry(whichCountry)
+  }
+
+  const rows = () => (countryToShow.length === 1) 
+    ? countryToShow.map(country =>
+    <CountryFull 
+       key={country.name + country.capital}
+       country={country}
+    />
+   )
+   : countryToShow.map(country =>
+    <CountryLess
+       key={country.name + country.capital}
+       country={country} handleShowClick={handleShowClick(country.name)}
+    />
+   )
+
+
+  return(
+    <div>
+    <Filter filterLabel={'find countries'} handleSearchChange={handleSearchChange} />
+    <Countries countries={rows()} />
+    </div>
+    
+  )
+}
+
+export default App
---
+++ part2/data-for-countries/src/components/Weather.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Weather = ({ capital }) => {
+    const [location, setLocation] = useState('')
+    const [weather, setWeather] = useState('')
+    const [wind, setWind] = useState('')
+    const [windDir, setWindDir] = useState('')
+    const [weatherIcon, setWeatherIcon] = useState('')
+    // const [todos, setTodos] = useState([{ }]);
+   // console.log('weatherprops', capital)
+   const params = {
+    access_key: '7a67f03fb3ab41801c1e250c11e6f880',
+    query: capital
+}
+    useEffect(() => {
+  
+        console.log('effect')
+        axios
+        .get('http://api.weatherstack.com/current', {params})
+        .then(response => {
+            const apiResponse = response.data
+    //      console.log(apiResponse.current.temperature)
+    //      console.log(apiResponse.current.weather_icons[0])
+            setLocation(apiResponse.location.name)
+            setWeather(apiResponse.current.temperature)
+            setWind(apiResponse.current.wind_speed)
+            setWindDir(apiResponse.current.wind_dir)
+            setWeatherIcon(apiResponse.current.weather_icons[0])
+        //     setTodos({ name: apiResponse.location.name,
+        //             temperature: apiResponse.temperature,
+        //             wind_speed: apiResponse.wind_speed,
+        //             wind_dir: apiResponse.wind_dir,
+        //             weather_icons: apiResponse.weather_icons[0]             
+        // })
+        
+        })
+    }, [params])
+                
+      //const currentweather = weather.current
+      //console.log('temperature', Object.keys(currentweather))
+    //   axios.get('https://api.weatherstack.com/current', {params})
+    //   .then(response => {
+    //     const apiResponse = response.data
+    //     console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`)
+    //   }).catch(error => {
+    //     console.log(error)
+    //   })
+
+    return (
+        <div>
+            <h3>Weather in {location}</h3>
+            <p><b>temperature:</b> {weather} Celsius </p>
+            <img alt="weather-icon" width="50" height="50" src={weatherIcon}></img>
+            <p><b>wind:</b> {wind} kph direction {windDir}</p>
+            {/* <h4>Weather in {todos.name}</h4> */}
+        </div>
+    )
+}
+
+export default Weather
