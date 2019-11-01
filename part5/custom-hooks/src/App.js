bb220dee52346e473576e73e40b82df7b23ce456
ranabhat <paribessh_sab@hotmail.com>
2019-11-01 22:26:07 +0200

    ultimate hook 5.21 complete set up

--- part5/custom-hooks/src/App.js
+++ part5/custom-hooks/src/App.js
@@ -5,26 +5,27 @@ const useField = (type) => {
   const [value, setValue] = useState('')
 
   const onChange = (event) => {
+    if (!event) {
+      setValue('')
+    }
+    else if (event) {
     setValue(event.target.value)
   }
-
-  const reset = () => {
-    setValue('')
   }
 
   return {
     type,
     value,
-    onChange,
-    reset
+    onChange
+   // reset
   }
 }
 
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
  // console.log('resources', resources)
- 
-  // callback to prevent react-hooks/exhaustive-deps
+
+  // callback use to prevent react-hooks/exhaustive-deps
   const getAll = useCallback(async() => {
     const request = await axios.get(baseUrl)
     return  request.data
@@ -57,35 +58,36 @@ const App = () => {
   const content = useField('text')
   const name = useField('text')
   const number = useField('text')
-  const notes = useResource('http://localhost:3005/notes')
-  const persons = useResource('http://localhost:3005/persons')
-  //const [notes, noteService] = useResource('http://localhost:3005/notes')
-  //const [persons, personService] = useResource('http://localhost:3005/persons')
-  console.log('notes', notes[0])
-  console.log('persons', persons[0])
-
-
+  const notes = useResource('http://localhost:3006/notes')
+  const persons = useResource('http://localhost:3006/persons')
+  // console.log('notes', notes[0])
+  // console.log('persons', persons[0])
+  
   const handleNoteSubmit = (event) => {
     event.preventDefault()
     notes[1].create({ content: content.value })
+    content.reset = () => content.onChange('')
     content.reset()
   }
     
   const handlePersonSubmit = (event) => {
     event.preventDefault()
     persons[1].create({ name: name.value, number: number.value})
+    name.reset = () => name.onChange('')
+    number.reset = () => number.onChange('')
     name.reset()
     number.reset()
   }
   
+
   return (
     <div>
       <h2>notes</h2>
       <form onSubmit={handleNoteSubmit}>
-        <input //{...content} 
-          type={content.type}
-          value={content.value}
-          onChange={content.onChange}
+        <input {...content} 
+          // type={content.type}
+          // value={content.value}
+          // onChange={content.onChange}
         />
         <button>create</button>
       </form>
@@ -93,7 +95,7 @@ const App = () => {
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input //{...name.value}
+        name <input //{...name}
                  type={name.type}
                  value={name.value}
                  onChange={name.onChange}
